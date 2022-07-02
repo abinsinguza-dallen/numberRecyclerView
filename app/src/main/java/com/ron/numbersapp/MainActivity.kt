e82c598f3afd7fd010b030e3327e0f8d5dc3943f
-abinsinguza-dallen <dallenabinsinguza@gmail.com>
2022-07-02 07:58:14 +0300

    first commit

--- app/src/main/java/com/ron/numbersapp/MainActivity.kt
+++ app/src/main/java/com/ron/numbersapp/MainActivity.kt
@@ -6,17 +6,32 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.ron.numbersapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding:ActivityMainBinding
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding= ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        displaynumbers()
-    }
-    fun displaynumbers(){
-        val numberList= listOf(0,1,1,2,3,5,8,13,21,34,55,99)
+        var numbersList=Addition(100)
+        print(numbersList)
         binding.rvNumber.layoutManager=LinearLayoutManager(this)
-        val numbersAdaptor=NumberRecyclerViewAdaptor(numberList)
-        binding.rvNumber.adapter=numbersAdaptor
+        binding.rvNumber.adapter=NumberRecyclerViewAdaptor(numbersList)
+
+    }
+
+    fun Addition (size:Int):List<Int>{
+
+        var list = ArrayList<Int>()
+        var num1 = 1
+        var num2 = 0
+        var b = 0
+        while (b <= size) {
+            print(num1)
+            var sum = num1 + num2
+            num1 = num2
+            num2 = sum
+            b++
+            list += sum
+        }
+        return list
     }
 }
